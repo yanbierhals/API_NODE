bf6617a6da1429256170bf7761405e3e7a38fb47
YAN DA ROSA BIERHALS <181700506@fspoa.br>
2024-03-26 21:35:44 -0300

    Produto

---
+++ src/controller/produto_controller.ts
@@ -0,0 +1,67 @@
+import { StatusCodes } from "http-status-codes";
+import { Request, Response } from 'express';
+
+const produtoService = require('../service/produto_service')
+
+
+async function listar(req: Request, res: Response) {
+    const listaProdutos = await produtoService.listar();
+    res.json(listaProdutos);
+}
+
+async function inserir(req: Request, res: Response) {
+    let produto = req.body;
+    try {
+        const prodInserido = await produtoService.inserir(produto);
+        res.status(StatusCodes.OK).json(prodInserido);
+    }
+    catch(err: any) {
+        //id-> 400 / msg -> msg de erro
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message});
+    }
+}
+
+
+async function buscarPorId(req: Request, res: Response): Promise<void> {
+    const id = +req.params.id;
+    try {
+      const prod = await produtoService.buscarPorId(id);
+      res.json(prod);
+    }
+    catch(err: any) {
+      //id-> 404 / msg -> msg de erro
+      res.status(err.id).json({msg: err.message});
+    }
+}
+
+async function atualizar (req: Request, res: Response) {
+    const id = +req.params.id;
+    let produto = req.body;
+  
+    try{ 
+      const produtoAtualizado = await produtoService.atualizar(id, produto);
+      res.json(produtoAtualizado);
+    }
+    catch(err: any) {
+      res.status(err.id).json({msg: err.message});
+    }
+}
+
+async function deletar(req: Request, res: Response) {
+    const id = +req.params.id;
+    try{ 
+      const produtoDeletado = await produtoService.deletar(id);
+      res.json(produtoDeletado);
+    }
+    catch(err: any) {
+      res.status(err.id).json({msg: err.message});
+    }   
+}
+
+module.exports = {
+    listar,
+    inserir,
+    buscarPorId,
+    atualizar,
+    deletar
+}
---
+++ src/service/produto_service.ts
@@ -0,0 +1,58 @@
+import { Produto } from "../model/produto.interface";
+
+const produtoRepository = require('../repository/produto_repository')
+
+function listar() {
+    return produtoRepository.listar();
+}
+
+function inserir(produto: Produto) {
+    if(produto && produto.nome && produto.preco) {// produto != undefined
+        produtoRepository.inserir(produto);
+    }
+    else {
+        throw {id:400, message:"Produto nao possui nome ou preco"};
+    }
+}
+
+function buscarPorId(id: number) {
+    const produto = produtoRepository.buscarPorId(id);
+    if(produto) {
+        return produto;
+    }
+    else {
+        throw {id:404, message:"Produto nao encontrado"};
+    }
+}
+
+function atualizar(id: number, produtoAtualizado: Produto) {
+    const produto = produtoRepository.buscarPorId(id);
+    if(!produto) {
+        throw {id: 404, message: "Produto nao encontrado"};
+    }
+    
+    if(produtoAtualizado && produtoAtualizado.nome && produtoAtualizado.preco){
+        produtoRepository.atualizar(id, produtoAtualizado);
+    }
+    else {
+        throw {id: 400, message: "Produto nao possui um dos campos obrigatorios"};
+    }
+}
+
+function deletar(id: number) {
+    const produtoDeletado = produtoRepository.deletar(id);
+    if(produtoDeletado){
+        return produtoDeletado;
+    }
+    else {
+        throw {id: 404, message: "Produto nao encontrado"};
+    }
+}
+
+module.exports = {
+    listar,
+    inserir,
+    buscarPorId,
+    atualizar,
+    deletar
+}
